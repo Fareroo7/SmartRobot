51a746c17b9f463be0270767944b920642b61eae
recke96 <jakob.ecker@gmx.at>
2014-05-08 16:44:47 +0200

    created package at.htl.Visualisation for GUIs needed to show results and test created class GUI_Trilateration

---
+++ SmartBot/src/at/htl/Visualisation/GUI_Trilateration.java
@@ -0,0 +1,42 @@
+package at.htl.Visualisation;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GUI_Trilateration extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_Trilateration frame = new GUI_Trilateration();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI_Trilateration() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
