92374562688bd0dac1388dc29cc3fe952468d119
Fareroo7 <Fareroo7@gmail.com>
2014-05-13 17:56:19 +0200

    ...

---
+++ SmartBot/src/at/htl/smartbot/Test.java
@@ -0,0 +1,68 @@
+package at.htl.smartbot;
+
+/**
+ * Test Class
+ * 
+ * @author Jakob Ecker & Dominik Simon
+ * 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+
+		// Point m1 = new Point(1, 1);
+		// Point m2 = new Point(3, 1);
+		// Point m3 = new Point(1.5, 3);
+		//
+		// Point t1= new Point(0.15,1.53);
+		// Point t2= new Point(2,1.06);
+		// Point t3= new Point(2.25,1);
+		// Point t4= new Point(2.7,1.4);
+		//
+		// // Trilateration.trilaterate(m1, m2, m3, 1, 0.5, 2);
+		//
+		// Triangle test = new Triangle(m1,m2,m3);
+		//
+		// System.out.println(test.toString());
+
+		// Point p = Line.getPointsOfIntersectionLine(new Line(new Point(1, 1),
+		// new Point(6, 4)), new Line(new Point(2, 4), new Point(6, 2)));
+		// System.out.println(p.toString());
+
+		// ArrayList<Point> points = new ArrayList<Point>();
+		// points.add(new Point(2, 1));
+		// points.add(new Point(3, 3));
+		// points.add(new Point(6, 2));
+		//
+		// Triangle t = Triangle.getSmallestTriangel(points);
+		//
+		// Point s = Triangle.getCentroidOfSmallestTriangle(t);
+		//
+		// System.out.println(s.toString());
+
+		// Point s1 = new Point(1,1);
+		// Point s2 = new Point(1,4);
+		// Point s3 = new Point(4,1);
+		//
+		// Point position = Trilateration.trilaterate(s1, s2, s3, 2.0, 2.0,
+		// 2.5);
+		//
+		// System.out.println(position.toString());
+		//
+		// Line l1 = new Line(new Point(1, 0), new Point(1.1,3));
+		// Line l2 = new Line(new Point(0.5,1), new Point(3,3));
+		//
+		// Point s = Line.getPointsOfIntersectionLine(l1, l2);
+		//
+		// System.out.println(s.toString());
+
+		Point t1 = new Point(1, 1);
+		Point t2 = new Point(2, 2);
+		Point t3 = new Point(1, 2);
+
+
+		
+		Point p = Trilateration.trilaterate(t1, t2, t3, 1, 1, 1);
+		System.out.println(p);
+	}
+}
