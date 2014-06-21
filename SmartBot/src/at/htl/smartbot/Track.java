5ebce0e31709ec3817ac1a8f9438988c2345f0cd
recke96 <jakob.ecker@gmx.at>
2014-06-21 17:47:34 +0200

    added class Track 	added basic methods

---
+++ SmartBot/src/at/htl/smartbot/Track.java
@@ -0,0 +1,26 @@
+package at.htl.smartbot;
+
+import java.util.ArrayList;
+
+import at.htl.geometrics.*;
+
+public class Track {
+
+	private ArrayList<Point> track = new ArrayList<Point>();
+	
+	public Point getLastPosition(){
+		return track.get(track.size()-1);
+	}
+	
+	public Point getFirstPosition(){
+		return track.get(0);
+	}
+	
+	public Point getPosition(int index){
+		return track.get(index);
+	}
+	
+	public void addNavPoint(Point navigationPoint){
+		track.add(navigationPoint);
+	}
+}
