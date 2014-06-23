16061972f3f604216f9a41f18a3f4c1bcbdb44e5
recke96 <jakob.ecker@gmx.at>
2014-06-23 19:26:26 +0200

    new method sbtExport

--- SmartBot/src/at/htl/xml/SBTTranslator.java
+++ SmartBot/src/at/htl/xml/SBTTranslator.java
@@ -1,5 +1,21 @@
 package at.htl.xml;
 
+import java.io.*;
+
 public class SBTTranslator {
 
+	public static boolean sbtExport(File destinationFile, SBTHead header, SBTData data){
+		try {
+			BufferedWriter sbtExport = new BufferedWriter(new FileWriter(destinationFile));
+			sbtExport.write(header.toXMLString());
+			sbtExport.write(data.toXMLString());
+			sbtExport.close();
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 }
