7efdb50d12bd8ed66a35765933526d1c25ea7373
Fareroo7 <Fareroo7@gmail.com>
2014-07-19 18:08:35 +0200

    CartesionVerctor toString added

--- SmartBot/src/at/htl/smartbot/Test.java
+++ SmartBot/src/at/htl/smartbot/Test.java
@@ -16,8 +16,11 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println(Trilateration.getPointsOfIntersectionCrircleV2(new Point(5,5), new Point(3.5,3.5), 4, 1));
+		//Line l = Trilateration.getPointsOfIntersectionCrircleV2(new Point(5,5), new Point(3.5,3.5), 4, 1);
+		//System.out.println(l);
 		 
+		CartesianVector cv = new CartesianVector(2, 0);
+		System.out.println(cv.toPolarVektor());
 		
 	}
 }
