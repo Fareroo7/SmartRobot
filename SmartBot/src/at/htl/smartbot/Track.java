94f7c7331ddaf918d617d946e88955c282a44b22
recke96 <jakob.ecker@gmx.at>
2014-07-26 19:09:26 +0200

    added javadoc for class track

--- SmartBot/src/at/htl/smartbot/Track.java
+++ SmartBot/src/at/htl/smartbot/Track.java
@@ -4,34 +4,66 @@ import java.util.ArrayList;
 
 import at.htl.geometrics.*;
 
+/**
+ * Represent a track of our smartbot stored as a ArrayList of navigation points
+ * @author Jakob Ecker
+ * @author Domink Simon
+ *
+ */
 public class Track {
 	
 	private ArrayList<Point> track = new ArrayList<Point>();
 	
-	public Track(){
-		
+	/**
+	 * Constructs a new empty Track-Object
+	 */
+	public Track(){	}
+	
+	/**
+	 * Constructs a new Track-Object with a list of navigation points
+	 * @param navPoints
+	 */
+	public Track(ArrayList<Point> navPoints){
+		track = navPoints;
 	}
 	
-	public Track(ArrayList<Point> positions){
-		track = positions;
-	}
-	
+	/**
+	 * 
+	 * @return the last position of the track
+	 */
 	public Point getLastPosition(){
 		return track.get(track.size()-1);
 	}
 	
+	/**
+	 * 
+	 * @return the starting position of the track
+	 */
 	public Point getFirstPosition(){
 		return track.get(0);
 	}
 	
+	/**
+	 * 
+	 * @param index
+	 * @return the position of the track at index
+	 */
 	public Point getPosition(int index){
 		return track.get(index);
 	}
 	
+	/**
+	 * add a new navigation point to the track, added as the last position
+	 * @param navigationPoint
+	 */
 	public void addNavPoint(Point navigationPoint){
 		track.add(navigationPoint);
 	}
 	
+	/**
+	 * Getter
+	 * @return
+	 */
 	public ArrayList<Point> getAllPositions(){
 		return track;
 	}
