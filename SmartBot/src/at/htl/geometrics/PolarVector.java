f5e733aa4b0616dbf648155ef8b243e3a3695862
recke96 <jakob.ecker@gmx.at>
2014-07-26 18:14:18 +0200

    added javadoc to class PolarVector added default constructor to PolarVector

--- SmartBot/src/at/htl/geometrics/PolarVector.java
+++ SmartBot/src/at/htl/geometrics/PolarVector.java
@@ -2,14 +2,29 @@ package at.htl.geometrics;
 
 import at.htl.smartbot.Utils;
 
+/**
+ * Represents a vector in polar form (magnitude z and angle &phi;)
+ * @author Jakob Ecker
+ * @author Domink Simon
+ */
 public class PolarVector {
 	private double z;
 	private double phi;
 	
+	/**
+	 * Constructs a new polar vector with magnitude z and angle &phi;
+	 * @param z
+	 * @param phi
+	 */
 	public PolarVector(double z, double phi){
 		this.z = z;
 		this.phi = phi;
 	}
+	
+	/**
+	 * Constructs a new empty polar vector
+	 */
+	public PolarVector(){};
 	
 	public CartesianVector toCartesianVector(){
 		double x = this.z * Math.cos(this.phi);
@@ -17,18 +32,34 @@ public class PolarVector {
 		return new CartesianVector(x, y);
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getZ() {
 		return z;
 	}
 
+	/**
+	 * Setter
+	 * @param z
+	 */
 	public void setZ(double z) {
 		this.z = z;
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getPhi() {
 		return phi;
 	}
 
+	/**
+	 * Setter
+	 * @param z
+	 */
 	public void setPhi(double phi) {
 		this.phi = phi;
 	}
