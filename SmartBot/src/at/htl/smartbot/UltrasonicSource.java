78b9cf40164644e0cc6255b03c784a15f85d435d
recke96 <jakob.ecker@gmx.at>
2014-07-27 17:33:20 +0200

    added javadoc for class UltrasonicSource removed final modifier for position

--- SmartBot/src/at/htl/smartbot/UltrasonicSource.java
+++ SmartBot/src/at/htl/smartbot/UltrasonicSource.java
@@ -3,44 +3,84 @@ package at.htl.smartbot;
 import at.htl.geometrics.Point;
 
 /**
- * Class that stores Position of and distance to an ultrasonic nois source. Additionally the time of the last distance measurement is stored
- * @author Jakob Ecker , Dominik Simon
+ * Represents a ultrasonic source hardware and stores its position, distance and the time of the last update
+ * @author Jakob Ecker
+ * @author Dominik Simon
  *
  */
 public class UltrasonicSource {
 
-	private final Point pos;
+	private Point pos;
 	private double distance;
 	private double time_of_last_measure;
 
+	/**
+	 * Constructs a new UltrasonicSource with the position
+	 * @param pos
+	 */
 	public UltrasonicSource(Point pos) {
 		this.pos = pos;
 	}
 
+	/**
+	 * Constructs a new empty UltrasonicSource
+	 */
+	public UltrasonicSource(){
+		
+	}
+	
+	/**
+	 * Getter
+	 * @return
+	 */
 	public Point getPos() {
 		return pos;
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getPos_X() {
 		return pos.getX();
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getPos_Y() {
 		return pos.getY();
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getDistance() {
 		return distance;
 	}
 
+	/**
+	 * Setter
+	 * @return
+	 */
 	public void setDistance(double distance) {
 		this.distance = distance;
 	}
 
+	/**
+	 * Getter
+	 * @return
+	 */
 	public double getTime_of_last_measure() {
 		return time_of_last_measure;
 	}
 
+	/**
+	 * Setter
+	 * @return
+	 */
 	public void setTime_of_last_measure(double time_of_last_measure) {
 		this.time_of_last_measure = time_of_last_measure;
 	}
