b069f1a53bea9126dbc83d5a371014488b8eac73
Dominik Simon <fareroo7@gmail.com>
2014-09-11 21:21:43 +0200

    ...

--- SmartBot/src/at/htl/smartbot/Test.java
+++ SmartBot/src/at/htl/smartbot/Test.java
@@ -11,9 +11,6 @@ import at.htl.ECP.ECP;
 public class Test {
 
 	public static void main(String[] args) {
-		byte c = (byte)ECP.getECMessage(true, 100, 255).charAt(2);
-	
-		System.out.println((Integer.toBinaryString(c)));
 		
 	}
 }
