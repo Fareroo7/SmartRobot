fb7c0f3b95adafaa98545c87db10ed411589c8dd
recke96 <jakob.ecker@gmx.at>
2014-09-17 09:37:08 +0200

    javadoc add

--- SmartBot/src/at/htl/smartbot/EnvironmentalParameter.java
+++ SmartBot/src/at/htl/smartbot/EnvironmentalParameter.java
@@ -1,8 +1,16 @@
 package at.htl.smartbot;
 
+/**
+ * Defines and calculates environmental parameters
+ * Primary the change of the acoustic velocity with the temperature. 
+ * 
+ * @author Jakob Ecker
+ * @author Domink Simon
+ *
+ */
 public class EnvironmentalParameter {
 
-	/*
+	/**
 	 * Acoustic velocity in air for temperature is 0°C
 	 */
 	private static final double ACOUSTIC_VELOCITY_0 = 331.5;
@@ -12,14 +20,27 @@ public class EnvironmentalParameter {
 	// air temperature in °C
 	private static double air_temperature = 20;
 
+	/**
+	 * 
+	 * @return The acoustic velocity for the air temperature set in setAirTemperature(...) <br /> The default value for temperature is 20°C
+	 * 
+	 */
 	public static double getAcousticVelocity() {
 		return ACOUSTIC_VELOCITY_0 * Math.sqrt(1 + EXPANSION_COEFFICIENT * air_temperature);
 	}
 
+	/**
+	 * Getter
+	 * @return air_temperature
+	 */
 	public static double getAirTemperature() {
 		return air_temperature;
 	}
 
+	/**
+	 * Setter
+	 * @param air_temperature
+	 */
 	public static void setAirTemperature(double air_temperature) {
 		EnvironmentalParameter.air_temperature = air_temperature;
 	}
