ff7754c38b96a74b8a49fb398d5719fff0f62d1f
Dominik Simon <fareroo7@gmail.com>
2014-09-23 19:43:34 +0200

    javadoc: ECP

--- SmartBot/src/at/htl/ECP/ECP.java
+++ SmartBot/src/at/htl/ECP/ECP.java
@@ -9,7 +9,13 @@ package at.htl.ECP;
 public class ECP {
 	
 	
-
+	/**
+	 * Returns a ECPMessage as {@link String}.
+	 * @param forward Directions flag (true = forward and false = backward).
+	 * @param left Duty Circle for motors on the left side.
+	 * @param right Duty Circle for motors on the right side.
+	 * @return A ECPMessage as {@link String}.
+	 */
 	public static String getECMessage(boolean forward, int left, int right){
 		return ECPParameter.START + (forward ? ECPParameter.DIRECTION_FORWARD : ECPParameter.DIRECTION_BACKWARD) + (char)left + (char)right + ECPParameter.END ;
 	}
