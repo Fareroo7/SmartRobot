bd728a5f695cd7f3d5c67ff63c891302c3806b12
recke96 <jakob.ecker@gmx.at>
2014-09-24 15:09:15 +0200

    update ECP to version 2.1, inc. javadoc

--- SmartBot/src/at/htl/EngineControl/ECP.java
+++ SmartBot/src/at/htl/EngineControl/ECP.java
@@ -2,9 +2,9 @@ package at.htl.EngineControl;
 
 
 /**
- * Represents a ECP Message. All possible parameters can be added and the mandatory parameters Start and Stop are added automatically.
+ * Defines methodes returning the correct {@link String}s for communication with Engine control arduino
  * @author Dominik Simon
- * @version 1.0
+ * @version 2.1
  */
 public class ECP {
 	
@@ -15,11 +15,22 @@ public class ECP {
 	 * @param left Duty Circle for motors on the left side.
 	 * @param right Duty Circle for motors on the right side.
 	 * @return A ECPMessage as {@link String}.
+	 * @deprecated with version 2.0
 	 */
 	public static String getECMessage(boolean forward, int left, int right){
 		return ECPParameter.START + (forward ? ECPParameter.DIRECTION_FORWARD : ECPParameter.DIRECTION_BACKWARD) + (char)left + (char)right + ECPParameter.END ;
 	}
 	
-	
+	/**
+	 * 
+	 * Returns the ECP-Message as {@link String} for version 2.1.
+	 * @param directionCode The direction code from {@link ECPParameter}.
+	 * @param leftDutyCycle Duty Cycle for left engines.
+	 * @param rightDutyCycle Duty Cycle for right engines.
+	 * @return ECP-Message as {@link String}.
+	 */
+	public static String getECMessage(byte directionCode, int leftDutyCycle, int rightDutyCycle){
+		return ECPParameter.START+(char)directionCode + (char) leftDutyCycle + (char) rightDutyCycle + ECPParameter.END;
+	}
 	
 }
