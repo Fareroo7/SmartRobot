d0d2410d5089abbae759fba3257eed87db2db10a
recke96 <jakob.ecker@gmx.at>
2014-10-22 11:10:03 +0200

    add udp-receiver

--- SmartRobotServer/src/at/htl/smartrobot/server/Receiver.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/Receiver.java
@@ -1,5 +1,29 @@
 package at.htl.smartrobot.server;
 
-public class Receiver {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+public class Receiver extends Thread{
+
+	@Override
+	public void run() {
+		try {
+			DatagramSocket socket = new DatagramSocket(50000);
+
+			while (!this.isInterrupted()) {
+				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+				socket.receive(packet);
+			}
+
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		super.run();
+	}
 }
