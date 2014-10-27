cf8c081ed49ce6cc8849a67db6a12b3614d9f33e
Dominik Simon <fareroo7@gmail.com>
2014-10-27 15:03:24 +0100

    add timertask

--- SmartRobotServer/src/at/htl/smartrobot/server/test_send.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/test_send.java
@@ -8,10 +8,23 @@ import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.sql.Time;
 import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class test_send {
+	
+	public static long sendTime;
+	public static long receiveTime;
+	public static long timeOffset;
+	
+	public static InetAddress mInetAddress;
+	public static int port;
+	public static DatagramSocket socket;
+	public static byte[] data = {(byte)'s'};
+	public static DatagramPacket packet; 
 	
 	public static void main(String[] args) {
 		Receiver r = new Receiver(50100, 8);
@@ -20,22 +33,67 @@ public class test_send {
 
 			@Override
 			public void onReceive(UDPReceiveEvent e) {
-				System.out.println(ByteUtils.bytesToLong(e.getUdpPacket().getData()));
+				receiveTime = System.nanoTime();
+				long clientTime = ByteUtils.bytesToLong(e.getUdpPacket().getData());
+				
+				timeOffset = receiveTime - (clientTime + ((receiveTime - sendTime) / 2));
+				
+				System.out.println("gesendet: \t" + sendTime);
+				System.out.println("empfangen: \t" + receiveTime);
+				System.out.println("clientTime: \t" + clientTime);
+				System.out.println("offset: \t" + timeOffset);
+				
 			}
 
 		}
 
 		r.addUDPReceiveListener(new ReceivedListener());
 		
+		class SendTask extends TimerTask {
+
+			@Override
+			public void run() {
+				try {
+					
+					mInetAddress = InetAddress.getByName("10.22.2.208");
+					port = 50006;
+					
+					packet = new DatagramPacket(data, data.length, mInetAddress, port);
+					socket = new DatagramSocket();
+					
+					sendTime = System.nanoTime();
+					
+					socket.send(packet);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SocketException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
 		try {
-			InetAddress mInetAddress = InetAddress.getByName("10.0.0.37");
+			InetAddress mInetAddress = InetAddress.getByName("10.22.2.208");
 			int port = 50006;
 //			byte[] data = ByteBuffer.allocate(8).putLong(System.nanoTime()).array();
-			byte[] data = {(byte)'s'};
-			DatagramPacket packet = new DatagramPacket(data, data.length, mInetAddress, port);
-			DatagramSocket socket = new DatagramSocket();
+			packet = new DatagramPacket(data, data.length, mInetAddress, port);
+			socket = new DatagramSocket();
+			
+			sendTime = System.nanoTime();
+			
 			socket.send(packet);
 			
+			Timer timer = new Timer();
+			
+			timer.schedule(new SendTask(), 500, 500);
+			
+			socket.close();
 			r.run();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -47,6 +105,7 @@ public class test_send {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
 }
