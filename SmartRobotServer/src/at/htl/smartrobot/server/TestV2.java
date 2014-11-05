ab55a8e64e5fc8b15921e44b0ba30603131187ac
Dominik Simon <fareroo7@gmail.com>
2014-11-05 10:13:27 +0100

    ...

--- SmartRobotServer/src/at/htl/smartrobot/server/TestV2.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/TestV2.java
@@ -4,17 +4,17 @@ public class TestV2 {
 
 	
 	public static void main(String[] args) {
-		
+		System.out.println(fib(700));
 	}
 	
-	public static int fib(int n){
-		int pre = -1;
-		int res = 1;
+	public static long fib(long n){
+		long pre = -1;
+		long res = 1;
 		
-		for(int i = 0; i <= n; i++){
-			int sum = res + pre;
+		for(long i = 0; i <= n; i++){
+			long sum = res + pre;
 			pre = res;
-			res = sum;
+			res = sum;			
 		}
 		
 		return res;
