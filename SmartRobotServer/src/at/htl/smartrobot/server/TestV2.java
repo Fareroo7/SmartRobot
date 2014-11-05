d990b2481d96ff21b29aebdc5076a0bae10695e8
recke96 <jakob.ecker@gmx.at>
2014-11-05 09:21:20 +0100

    test

--- SmartRobotServer/src/at/htl/smartrobot/server/TestV2.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/TestV2.java
@@ -3,6 +3,15 @@ package at.htl.smartrobot.server;
 public class TestV2 {
 
 	
+	private int runs = 5000;
+	private long[] offset_Timer = new long[runs];
+	long systemTimer_before;
+	long systemTimer_after;
+	long nanoTimer_before;
+	long nanoTimer_after;
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println(fib(700));
 	}
