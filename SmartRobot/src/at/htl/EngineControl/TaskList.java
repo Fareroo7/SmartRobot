fe5c0c5fb7c60da6c25bcc6a5af8f54251338574
recke96 <jakob.ecker@gmx.at>
2014-11-18 08:39:11 +0100

    try tasklist

--- SmartRobot/src/at/htl/EngineControl/TaskList.java
+++ SmartRobot/src/at/htl/EngineControl/TaskList.java
@@ -1,7 +1,33 @@
 package at.htl.EngineControl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TaskList {
 
+	private LinkedList<EngineTask> tasks = new LinkedList<EngineTask>();
+	
+	public TaskList(){
+	}
+	
+	public int addNewTask(EngineTask task){
+		tasks.add(task);
+		return tasks.indexOf(task);
+	}
+	
+	public boolean removeTask(int id){
+		
+		tasks.remove(id);
+		
+		return false;
+	}
+	
+	public void taskComplete(){
+		
+	}
 	
 	
 }
