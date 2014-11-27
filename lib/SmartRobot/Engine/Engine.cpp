86fe2d89929437dd8af96708928c466cf4bf7284
Dominik Simon <fareroo7@gmail.com>
2014-11-27 21:47:31 +0100

    add documentation

--- lib/SmartRobot/Engine/Engine.cpp
+++ lib/SmartRobot/Engine/Engine.cpp
@@ -6,9 +6,14 @@
 #include "Arduino.h"
 #include "Engine.h"
 
-/*
-   Constructs a new EnginTask from the received data.
-*/
+//Constructs a new empty Engine. (Used to declare "placeholder").
+Engine::Engine(){}
+
+//Constructs a new Engine.
+//Parameter:
+//  pinOne => first Input-Pin.
+//  pinTwo => second Input-Pin.
+//  pinDis => Disable-Pin.
 Engine::Engine(int pinOne, int pinTwo, int pinDis)
 {
   _pinOne = pinOne;
@@ -20,33 +25,40 @@ Engine::Engine(int pinOne, int pinTwo, int pinDis)
   digitalWrite(_pinDis, HIGH);
 }
 
-Engine::Engine(){}
-
+//Return the first Input-Pin of the H-bridge.
 int Engine::getPinOne()
 {
   return _pinOne;
 }
 
+//Return the second Input-Pin of the H-bridge.
 int Engine::getPinTwo()
 {
   return _pinTwo;
 }
 
+//Return the Disable-Pin of the H-bridge.
 int Engine::getPinDis()
 {
   return _pinDis;
 }
 
+ //Enable the Engine (Disable-Pin = low).
 void Engine::enable()
 {
   digitalWrite(_pinDis, LOW);
 }
 
+//Disable the Engine (Disable-Pin = high).
 void Engine::disable()
 {
   digitalWrite(_pinDis, HIGH);
 }
 
+//Set the PWM dutycycle for the Engine.
+//Parameter:
+//  forward => if true the Robot drive forward, else backward.
+//  dutyCycle => the dutycycle of the PWM wich controlls the speed of the motor. (min 0; max 255). 
 void Engine::setDutyCycle(bool forward, int dutyCycle)
 {
   if(forward)
