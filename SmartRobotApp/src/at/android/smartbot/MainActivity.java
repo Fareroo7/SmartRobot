01feb07aed06554d5194685bb21641bbff498df6
Dominik Simon <fareroo7@gmail.com>
2014-12-10 09:30:40 +0100

    ...

--- SmartRobotApp/src/at/android/smartbot/MainActivity.java
+++ SmartRobotApp/src/at/android/smartbot/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Toast;
 import at.htl.enginecontrol.EngineControl;
+import at.htl.smartbot.InertiaPositioning;
 
 
 public class MainActivity extends ActionBarActivity {
