5e757a85ae8c6aea7f1a4347271134e9c63090ba
Dominik Simon <fareroo7@gmail.com>
2014-12-12 11:41:05 +0100

    add interia pos.

---
+++ SmartRobotApp/src/at/android/smartbot/inertiapos/Clipper.java
@@ -0,0 +1,48 @@
+package at.android.smartbot.inertiapos;
+
+import at.htl.geometrics.CartesianVector;
+
+public class Clipper {
+
+	private int clippingSize = 10;
+	private CartesianVector[] values;
+	private CartesianVector sum;
+	private CartesianVector min;
+	private CartesianVector max;
+	private int pointer = 0;
+	private boolean isFull = false;
+
+	public Clipper(int clippingSize) {
+		super();
+		this.clippingSize = clippingSize;
+		this.values = new CartesianVector[this.clippingSize];
+		sum = new CartesianVector(0, 0);
+		min= new CartesianVector(Double.MIN_VALUE, Double.MIN_VALUE);
+		max = new CartesianVector(Double.MAX_VALUE,Double.MAX_VALUE);
+	}
+
+	public void add(CartesianVector v) {
+
+		if (isFull) {
+			sum.setX(sum.getX() - values[pointer].getX() + v.getX());
+			sum.setY(sum.getY() - values[pointer].getY() + v.getY());
+		} else {
+			sum.setX(sum.getX() + v.getX());
+			sum.setY(sum.getY() + v.getY());
+		}
+		
+		//insert Min-Max operation
+
+		values[pointer] = v;
+
+		if (pointer >= clippingSize - 1) {
+			pointer = 0;
+			isFull = true;
+		} else {
+			pointer++;
+		}
+	}
+	
+	
+
+}
