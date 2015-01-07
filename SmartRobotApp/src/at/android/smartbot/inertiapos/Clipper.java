42874ed614e53017a89e9ceadc28155b0756efaf
recke96 <jakob.ecker@gmx.at>
2015-01-07 09:11:49 +0100

    Update Clipper.java
    min max calculation - not finished

--- SmartRobotApp/src/at/android/smartbot/inertiapos/Clipper.java
+++ SmartRobotApp/src/at/android/smartbot/inertiapos/Clipper.java
@@ -17,8 +17,8 @@ public class Clipper {
 		this.clippingSize = clippingSize;
 		this.values = new CartesianVector[this.clippingSize];
 		sum = new CartesianVector(0, 0);
-		min= new CartesianVector(Double.MIN_VALUE, Double.MIN_VALUE);
-		max = new CartesianVector(Double.MAX_VALUE,Double.MAX_VALUE);
+		min = new CartesianVector(Double.MAX_VALUE,Double.MAX_VALUE);
+		max = new CartesianVector(Double.MIN_VALUE, Double.MIN_VALUE);
 	}
 
 	public void add(CartesianVector v) {
@@ -31,6 +31,30 @@ public class Clipper {
 			sum.setY(sum.getY() + v.getY());
 		}
 		
+		if(values[pointer].getX()==min.getX()){
+			min.setX(Double.MAX_VALUE);
+		}
+		if(values[pointer].getY()==min.getY()){
+			min.setY(Double.MAX_VALUE);
+		}
+		if(values[pointer].getX()==max.getX()){
+			max.setX(Double.MIN_VALUE);
+		}
+		if(values[pointer].getY()==max.getY()){
+			max.setY(Double.MIN_VALUE);
+		}
+		
+		values[pointer] = v;
+
+		if (pointer >= clippingSize - 1) {
+			pointer = 0;
+			isFull = true;
+		} else {
+			pointer++;
+		}
+	}
+	
+	private void checkMinMax(CartesianVector v){
 		if(v.getX()<min.getX()){
 			min.setX(v.getX());
 		}
@@ -42,19 +66,7 @@ public class Clipper {
 		}
 		if(v.getY()>max.getY()){
 			max.set(v.getY());
-		}
-		
-
-		values[pointer] = v;
-
-		if (pointer >= clippingSize - 1) {
-			pointer = 0;
-			isFull = true;
-		} else {
-			pointer++;
-		}
+		}	
 	}
-	
-	
 
 }
