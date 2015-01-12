28d5c1570b807974b8f0ee0583db5dd5df4c2be9
Dominik Simon <fareroo7@gmail.com>
2015-01-12 18:28:24 +0100

    ...

--- SmartRobotServer/src/at/htl/smartrobot/server/SimonTestSend.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/SimonTestSend.java
@@ -9,36 +9,57 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 
 import at.htl.smartrobot.server.utils.ByteUtils;
+import at.htl.smartrobot.server.utils.Receiver;
+import at.htl.smartrobot.server.utils.UDPReceiveEvent;
+import at.htl.smartrobot.server.utils.UDPReceiveListener;
 
-public class SimonTestSend {
+public class SimonTestSend implements UDPReceiveListener{
 	public static InetAddress mInetAddress;
 	public static int port;
 	public static DatagramSocket socket;
 	public static byte[] data = {(byte)'s'};
 	public static DatagramPacket packet; 
+	public static long sendTime;
+	public static long recevieTime;
+	public static long serverTime;
 	
 	public static void main(String[] args) {
-		for(int i = 0; i < 100; i++){
-			try {
-				
-				mInetAddress = InetAddress.getByName("10.0.0.51");
-				port = 50010;
-				
-				packet = new DatagramPacket(ByteUtils.longToBytes(System.nanoTime()), Long.SIZE / 8, mInetAddress, port);
-				socket = new DatagramSocket();
+		Receiver mReceiver = new Receiver(5042, Long.SIZE / 8);
+		mReceiver.addUDPReceiveListener(new SimonTestSend());
+		mReceiver.start();
+		
+		try {
 			
-//				sendTime = System.nanoTime();
+			mInetAddress = InetAddress.getByName("10.0.0.51");
+			port = 50010;
+			
+			packet = new DatagramPacket(ByteUtils.longToBytes(System.nanoTime()), Long.SIZE / 8, mInetAddress, port);
+			socket = new DatagramSocket();
+		
+			for(int i = 0; i < 10000; i++){
+				sendTime = System.nanoTime();
 				socket.send(packet);
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SocketException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Thread.sleep(100);
 			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void onReceive(UDPReceiveEvent e) {
+		recevieTime = System.nanoTime();
+		serverTime = ByteUtils.bytesToLong(e.getUdpPacket().getData());
+		System.out.println(recevieTime - sendTime + ";");
 	}
 }
