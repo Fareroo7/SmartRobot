28679ce3c736a29632d6416b7a2c5b5190c6b0eb
recke96 <jakob.ecker@gmx.at>
2015-01-28 14:10:20 +0100

    SmartServer and client

---
+++ SmartRobotServer/src/at/htl/smartrobot/server/SmartRuntime.java
@@ -0,0 +1,121 @@
+package at.htl.smartrobot.server;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.TimerTask;
+
+import at.htl.smartrobot.server.utils.Receiver;
+import at.htl.smartrobot.server.utils.UDPReceiveEvent;
+import at.htl.smartrobot.server.utils.UDPReceiveListener;
+
+public class SmartRuntime extends TimerTask implements UDPReceiveListener {
+
+	private int serverPort = 50099;
+	private int robotPort = 50100;
+
+	private Receiver mReceiver;
+
+	private InetAddress myAddress;
+	private InetAddress mRobotAddress;
+
+	private DatagramSocket robotSocket;
+
+	private DatagramPacket runtimeRequest;
+
+	private long avgRuntime = 0;
+	private int mCounter = 0;
+
+	// Log
+	private File logFile = new File("./logs.csv");
+	private int logCount = 0;
+	private BufferedWriter log;
+
+	private long timeAfter;
+	private long timeBefore;
+	private long timeSum;
+
+	public SmartRuntime() {
+		try {
+			myAddress = InetAddress.getByName("132.168.88.21");
+			mRobotAddress = InetAddress.getByName("132.168.88.22");
+			robotSocket = new DatagramSocket();
+			mReceiver = new Receiver(serverPort, 1);
+			runtimeRequest = new DatagramPacket("m".getBytes(), 1, mRobotAddress, robotPort);
+			log = new BufferedWriter(new FileWriter(logFile));
+			mReceiver.addUDPReceiveListener(this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public SmartRuntime(String serverAddress, String robotAddress, int serverPort, int robotPort) {
+		try {
+			myAddress = InetAddress.getByName(serverAddress);
+			mRobotAddress = InetAddress.getByName(robotAddress);
+			this.serverPort = serverPort;
+			this.robotPort = robotPort;
+			robotSocket = new DatagramSocket();
+			runtimeRequest = new DatagramPacket("m".getBytes(), 1, mRobotAddress, robotPort);
+			log = new BufferedWriter(new FileWriter(logFile));
+			mReceiver.addUDPReceiveListener(this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		avgRuntime = 0;
+		timeSum = 0;
+		try {
+			log.write("n;runtime[ns];new avg runtime [ns]");
+			log.newLine();
+			timeBefore = System.nanoTime();
+			robotSocket.send(runtimeRequest);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onReceive(UDPReceiveEvent e) {
+		String input = new String(e.getUdpPacket().getData());
+		switch (input) {
+		case "r":
+			timeAfter = System.nanoTime();
+			if (mCounter < 20) {
+				mCounter++;
+				try {
+					timeBefore = System.nanoTime();
+					robotSocket.send(runtimeRequest);
+					timeSum += (timeAfter - timeBefore) / 2;
+					avgRuntime = timeSum / mCounter;
+					log.write(logCount+";"+((timeAfter - timeBefore) / 2)+";"+avgRuntime);
+					log.newLine();
+					logCount++;
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			} else {
+				timeSum += (timeAfter - timeBefore) / 2;
+				avgRuntime = timeSum / (mCounter + 1);
+				mCounter = 0;
+			}
+
+			break;
+		}
+
+	}
+
+}
---
+++ SmartRobotServer/src/at/htl/smartrobot/server/SmartServer.java
@@ -0,0 +1,107 @@
+package at.htl.smartrobot.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.ArrayList;
+
+import at.htl.smartrobot.server.utils.*;
+import com.pi4j.io.gpio.*;
+
+public class SmartServer implements UDPReceiveListener {
+
+	public static final String RUNTIME_MEASURE = "m";
+
+	private Receiver mReceiver;
+	
+	private int serverPort = 50099;
+	private int robotPort = 50100;
+
+	private InetAddress myAddress;
+	private InetAddress mRobotAddress;
+
+	private DatagramSocket serverSocket;
+
+	private DatagramPacket runtimeRespond;
+	
+	
+	
+	private ArrayList<GpioPinDigitalOutput> pinList = new ArrayList<GpioPinDigitalOutput>();
+	private int nextPin = 0;
+
+	public SmartServer() {
+		try {
+			myAddress = InetAddress.getByName("132.168.88.21");
+			mRobotAddress = InetAddress.getByName("132.168.88.22");
+			serverSocket = new DatagramSocket();
+			mReceiver=new Receiver(serverPort,1);
+			runtimeRespond = new DatagramPacket("r".getBytes(), 1, mRobotAddress, robotPort);
+		} catch (UnknownHostException | SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public SmartServer(String serverAddress, String robotAddress, int serverPort, int robotPort) {
+		try {
+			myAddress = InetAddress.getByName(serverAddress);
+			mRobotAddress = InetAddress.getByName(robotAddress);
+			this.serverPort = serverPort;
+			this.robotPort = robotPort;
+			serverSocket = new DatagramSocket();
+			runtimeRespond = new DatagramPacket("r".getBytes(), 1, mRobotAddress, robotPort);
+		} catch (UnknownHostException | SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		SmartServer server = new SmartServer();
+		server.mReceiver.addUDPReceiveListener(server);
+	}
+	
+	public void addOutputPin(GpioPinDigitalOutput pin){
+		pinList.add(pin);
+	}
+
+	@Override
+	public void onReceive(UDPReceiveEvent e) {
+		String input = new String(e.getUdpPacket().getData());
+
+		switch (input) {
+		case "m":
+			try {
+				serverSocket.send(runtimeRespond);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			break;
+		case "a":
+			pinList.get(nextPin).setState(true);
+			try {
+				serverSocket.send(new DatagramPacket(("s"+nextPin).getBytes(), ("s"+nextPin).getBytes().length,mRobotAddress,robotPort));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			if(nextPin<pinList.size()){
+				nextPin++;	
+			}else{
+				nextPin=0;
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+
+}
