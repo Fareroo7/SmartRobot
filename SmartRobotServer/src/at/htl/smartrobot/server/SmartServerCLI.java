5b80839f34e6d24f8c85c9d38f97ddc05063832a
recke96 <jakob.ecker@gmx.at>
2015-02-11 11:20:53 +0100

    ...

--- SmartRobotServer/src/at/htl/smartrobot/server/SmartServerCLI.java
+++ SmartRobotServer/src/at/htl/smartrobot/server/SmartServerCLI.java
@@ -3,7 +3,6 @@ package at.htl.smartrobot.server;
 import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.Inet4Address;
