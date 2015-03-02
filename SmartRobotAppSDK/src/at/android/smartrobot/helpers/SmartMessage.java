2f21f5b33a405c6dc28f06f7edb4d55afa17087f
Dominik Simon <fareroo7@gmail.com>
2015-03-02 21:37:14 +0100

    add javadoc

--- SmartRobotAppSDK/src/at/android/smartrobot/helpers/SmartMessage.java
+++ SmartRobotAppSDK/src/at/android/smartrobot/helpers/SmartMessage.java
@@ -2,10 +2,26 @@ package at.android.smartrobot.helpers;
 
 import android.os.*;
 
+/**
+ * This is a helper for creating Messages for android handler.
+ * 
+ * @see Handler
+ * @author Dominik Simon
+ * @version 1.0
+ */
 public class SmartMessage {
 	
+	/**
+	 * The key for the message bundle data.
+	 */
 	public static final String BUNDLE_MESSAGE_KEY = "MESSAGE";
 	
+	/**
+	 * Creates a new Message.
+	 * @param what What type of message.
+	 * @param message The message data.
+	 * @return Message object.
+	 */
 	public static Message create(int what, String message){
 		Message m = new Message();
 		Bundle b = new Bundle();
