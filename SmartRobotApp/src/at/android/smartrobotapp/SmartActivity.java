cf50ec3404c4e13219a46d3951e75739a59b05e9
Dominik Simon <fareroo7@gmail.com>
2015-03-03 19:42:00 +0100

    add SmartHandler and tests

--- SmartRobotApp/src/at/android/smartrobotapp/SmartActivity.java
+++ SmartRobotApp/src/at/android/smartrobotapp/SmartActivity.java
@@ -1,34 +1,98 @@
 package at.android.smartrobotapp;
 
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import at.android.smartrobot.network.UDPController;
+import at.android.smartrobot.network.UDPReceiveEvent;
+import at.android.smartrobot.network.UDPReceiveListener;
 
-public class SmartActivity extends ActionBarActivity {
+public class SmartActivity extends ActionBarActivity implements UDPReceiveListener {
 
+	//UI
+	public Button btnSend;
+	
+	//Connections
+	//public USBController usbController = null;
+	public UDPController udpController = null;
+	
+	//Handler
+	public SmartHandler handler = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_smart);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.smart, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		handler = new SmartHandler(getApplicationContext());
+		
+		initUI();
+		
+		try {
+			//usbController = new USBController(getApplicationContext());
+			udpController = new UDPController(50001, 8, "10.0.0.17", 50000);
+		} catch (UnknownHostException | SocketException e) {
+			//TODO
+			e.printStackTrace();
 		}
-		return super.onOptionsItemSelected(item);
+		
+		udpController.addUDPReceiveListener(this);
+		udpController.startListening();
+		
+		btnSend.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try {
+					udpController.send('A');
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
 	}
+
+	private void initUI() {
+		btnSend = (Button) findViewById(R.id.btnSend);
+	}
+
+	@Override
+	public void onReceive(UDPReceiveEvent e) {
+		Toast.makeText(getApplicationContext(), "...", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		if(udpController != null) udpController.stopListening();
+		super.onDestroy();
+	}
+	
+	
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.smart, menu);
+//		return true;
+//	}
+
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
 }
---
+++ SmartRobotApp/src/at/android/smartrobotapp/SmartHandler.java
@@ -0,0 +1,40 @@
+package at.android.smartrobotapp;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+import at.android.smartrobot.network.UDPController;
+
+public class SmartHandler extends Handler{
+	
+	private Context context;
+	
+	public SmartHandler(Context context) {
+		this.context = context;
+	}
+
+	@Override
+	public void handleMessage(Message msg) {
+		
+		switch(msg.what){
+		
+		case UDPController.WHAT_SOCKET_IO_ERROR:
+		case UDPController.WHAT_SOCKET_RECEIVE_ERROR:
+			showMessage("UDPController error!");
+			break;
+		
+		case 1:
+			showMessage("Test");
+			break;
+		
+		}
+		
+		super.handleMessage(msg);
+	}
+
+	private void showMessage(String message){
+		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+	}
+	
+}
