2a1c6b760d6ab691eebefa73253e73a114ea23a0
Dominik Simon <fareroo7@gmail.com>
2015-03-03 21:30:12 +0100

    add audiocontroller

---
+++ SmartRobotAppSDK/src/at/android/smartrobot/audio/AudioController.java
@@ -0,0 +1,42 @@
+package at.android.smartrobot.audio;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public class AudioController extends Thread {
+
+	public static final int SAMPLE_RATE = 44100;
+
+	private AudioRecord audioRecorder;
+	private short[] buffer;
+	private boolean isRecording = false;
+
+	public AudioController() {
+		int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
+		buffer = new short[bufferSize];
+		audioRecorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+				AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+	}
+	
+	public void startListening(){
+		isRecording = true;
+		this.start();
+	}
+	
+	public void stopListening(){
+		isRecording = false;
+	}
+
+	@Override
+	public void run() {
+		while(isRecording){
+			int readSize = audioRecorder.read(buffer, 0, buffer.length);
+			
+		}
+	}
+	
+	
+
+}
