4dc155b9563e0dd7aa4d3197fe8ce7e98eb8250f
Dominik Simon <fareroo7@gmail.com>
2015-03-05 07:57:34 +0100

    ...

--- SmartRobotAppSDK/src/at/android/smartrobot/audio/AudioController.java
+++ SmartRobotAppSDK/src/at/android/smartrobot/audio/AudioController.java
@@ -8,14 +8,16 @@ import android.media.MediaRecorder;
 
 public class AudioController extends Thread {
 
+	public static final String TAG = "AudioController";
+
 	public static final int SAMPLE_RATE = 44100;
 
 	private AudioRecord audioRecorder;
 	private short[] buffer;
 	private boolean isRecording = false;
-	
+
 	private double periodicTime = 1 / SAMPLE_RATE;
-	
+
 	private ArrayList<AudioEventListener> listeners = new ArrayList<AudioEventListener>();
 
 	public AudioController() {
@@ -25,7 +27,7 @@ public class AudioController extends Thread {
 		audioRecorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
 				AudioFormat.ENCODING_PCM_16BIT, bufferSize);
 	}
-	
+
 	public void addSignalReceiveListener(AudioEventListener listener) {
 		listeners.add(listener);
 	}
@@ -34,49 +36,56 @@ public class AudioController extends Thread {
 		listeners.remove(listener);
 	}
 
-	private synchronized void notifyUSBReceived(AudioEvent e) {
+	public synchronized void notifySignalReceiveReceived(AudioEvent e) {
 		for (AudioEventListener l : listeners) {
 			l.onSignalReceive(e);
 		}
 	}
-	
-	public void startListening(){
+
+	public void startListening() {
 		isRecording = true;
+		audioRecorder.startRecording();
 		this.start();
 	}
-	
-	public void stopListening(){
+
+	public void stopListening() {
 		isRecording = false;
 	}
 
 	@Override
 	public void run() {
-		while(isRecording){
+		while (isRecording) {
 			int readSize = audioRecorder.read(buffer, 0, buffer.length);
 			long timestamp = System.nanoTime();
-			if(readSize > 0){
-				int signalOffset = analyseSignal(500);
-				if(signalOffset > 0){
-					timestamp -= (long)((buffer.length - signalOffset) * periodicTime);
-					notifyUSBReceived(new AudioEvent(getClass(), timestamp));
+			if (readSize > 0) {
+				int signalOffset = analyseSignal(10);
+				if (signalOffset > 0) {
+					timestamp -= (long) ((buffer.length - signalOffset) * periodicTime);
+					notifySignalReceiveReceived(new AudioEvent(getClass(), timestamp));
 				}
 			}
-			
+
 		}
 	}
 
 	private int analyseSignal(int minAmplitude) {
-		for(int i = 0; i < buffer.length - 1; i++){
-			if(buffer[i] > 0 && buffer[i + 1] < 0 && buffer[i + 2] > 0 && buffer[i + 3] < 0){
-				int amplitude = (int) Math.sqrt(buffer[i] * buffer[i] + buffer[i + 1] * buffer[i + 1] + buffer[i + 2] * buffer[i + 2] + buffer[i + 3] * buffer[i + 3]);
-				if(amplitude >= minAmplitude){
-					return i;
+		int numCrossing = 0;
+		for (int i = 0; i < buffer.length - 1; i++) {
+			if ((buffer[i] > 0 && buffer[i + 1] <= 0) || (buffer[i] < 0 && buffer[i + 1] >= 0)) {
+//				int amplitude = (int) Math.sqrt(buffer[i] * buffer[i] + buffer[i + 1] * buffer[i + 1] + buffer[i + 2]
+//						* buffer[i + 2] + buffer[i + 3] * buffer[i + 3]);
+//				if (amplitude >= minAmplitude) {
+//					return i;
+//				}
+				numCrossing ++;
+				if(numCrossing >= 10){
+					return i - 10;
 				}
+			} else {
+				numCrossing = 0;
 			}
 		}
 		return -1;
 	}
-	
-	
 
 }
