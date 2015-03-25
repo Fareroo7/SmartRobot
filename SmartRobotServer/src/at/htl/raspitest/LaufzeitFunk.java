93414ba3d299eaf1149b9645d0ea46428f958724
recke96 <jakob.ecker@gmx.at>
2015-03-25 09:47:47 +0100

    update test LaufzeitFunk

--- SmartRobotServer/src/at/htl/raspitest/LaufzeitFunk.java
+++ SmartRobotServer/src/at/htl/raspitest/LaufzeitFunk.java
@@ -10,23 +10,32 @@ import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class LaufzeitFunk {
-	
-	public static void main(String[] args){
+
+	static long risingEdge = 0;
+	static long fallingEdge = 0;
+
+	public static void main(String[] args) {
 		final GpioController gc = GpioFactory.getInstance();
 		final GpioPinDigitalInput ip = gc.provisionDigitalInputPin(RaspiPin.GPIO_02);
 		final GpioPinDigitalOutput op = gc.provisionDigitalOutputPin(RaspiPin.GPIO_01);
-		
+
 		ip.addListener(new GpioPinListenerDigital() {
-			
+
 			@Override
 			public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent arg0) {
-				if(arg0.getState()==PinState.HIGH){
-					op.pulse(20);
-				}	
+				if (arg0.getState() == PinState.HIGH) {
+					risingEdge = System.nanoTime();
+				} else {
+					fallingEdge = System.nanoTime();
+					long highTime = fallingEdge - risingEdge;
+					if (highTime > 750e3 && highTime < 1250e3) {
+						System.out.println("detec");
+					}
+				}
 			}
 		});
-		while(true);
+		while (true)
+			;
 	}
 
-	
 }
